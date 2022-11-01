172ca72508548e1a82273d4c933a7e34f02378e9
arina742 <arinazav02@gmail.com>
2022-11-01 16:17:56 +0300

    Add insertion sort and menu where you can choose bubble or insertion sort

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[] array = {3, 2 ,0, 8, 3, 4};
         System.out.println("This is array before sorting");
         printArray(array);
-        bubbleSorting(array);
+        int choice;
+        Scanner in = new Scanner(System.in);
+        System.out.println("1 - bubble sort \n" + "2 - insertion sort \n");
+        choice = in.nextInt();
+        switch (choice) {
+            case 1 -> bubbleSorting(array);
+            case 2 -> insertionSorting(array);
+        }
         System.out.println("This is array after sorting");
         printArray(array);
     }
@@ -11,13 +19,29 @@ public class Main {
     public static void bubbleSorting(int[] array){
         int temp;
         for(int i = 0; i < array.length; i++){
-            for(int j = 0; j < array.length - 1 - i; j++){
+            for(int j = 0; j < array.length - i - 1; j++){
                 if(array[j] > array[j+1]){
                     temp = array[j];
                     array[j] = array[j+1] ;
                     array[j+1] = temp;
                 }
             }
+        }
+    }
+
+    public  static void insertionSorting(int[] array){
+        for (int left = 0; left < array.length; left++){
+            int value = array[left];
+            int i = left - 1;
+            for(; i >=0; i--)
+            {
+                if(value < array[i]){
+                    array[i+1] = array[i];
+                }
+                else break;
+            }
+
+            array[i+1] = value;
         }
     }
     public static void printArray(int[] array) {
