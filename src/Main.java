cc3e1eca98492a36a9da4a33be72492b557fafc6
arina742 <arinazav02@gmail.com>
2022-11-01 16:09:24 +0300

    Created bubble sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {3, 2 ,0, 8, 3, 4};
+        System.out.println("This is array before sorting");
+        printArray(array);
+        bubbleSorting(array);
+        System.out.println("This is array after sorting");
+        printArray(array);
+    }
+
+    public static void bubbleSorting(int[] array){
+        int temp;
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length - 1 - i; j++){
+                if(array[j] > array[j+1]){
+                    temp = array[j];
+                    array[j] = array[j+1] ;
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void printArray(int[] array) {
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
     }
 }
