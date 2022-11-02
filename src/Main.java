15c845b0c6fcefe36bdc3e7e2d0283b272c7cab3
arina742 <arinazav02@gmail.com>
2022-11-02 20:34:03 +0300

    Изменена сортировка пузырьком на быструю сортировку

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int[] array = {3, 2 ,0, 8, 3, 4};
         System.out.println("This is array before sorting");
         printArray(array);
-        bubbleSorting(array);
+        int low = 0;
+        int high = array.length - 1;
+        quickSort(array, low, high);
         System.out.println("This is array after sorting");
         printArray(array);
     }
 
-    public static void bubbleSorting(int[] array){
-        int temp;
-        for(int i = 0; i < array.length; i++){
-            for(int j = 0; j < array.length - 1 - i; j++){
-                if(array[j] > array[j+1]){
-                    temp = array[j];
-                    array[j] = array[j+1] ;
-                    array[j+1] = temp;
+        public static void quickSort(int[] array, int low, int high) {
+
+            if (array.length == 0)
+                return;//завершить выполнение, если длина массива равна 0
+
+            if (low >= high)
+                return;//завершить выполнение если уже нечего делить
+
+            // выбрать опорный элемент
+            int middle = low + (high - low) / 2;
+            int opora = array[middle];
+
+            // разделить на подмассивы, который больше и меньше опорного элемента
+            int i = low, j = high;
+            while (i <= j) {
+                while (array[i] < opora) {
+                    i++;
+                }
+
+                while (array[j] > opora) {
+                    j--;
+                }
+
+                if (i <= j) {//меняем местами
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
                 }
             }
+
+            // вызов рекурсии для сортировки левой и правой части
+            if (low < j)
+                quickSort(array, low, j);
+
+            if (high > i)
+                quickSort(array, i, high);
         }
-    }
+
+
     public static void printArray(int[] array) {
         for (int j : array) {
             System.out.print(j + " ");
